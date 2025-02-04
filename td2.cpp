963df73dcb7274617a2ae6581767dfb6765a575b
mrHaubama <martinesboetos@gmail.com>
2025-02-04 17:16:44 -0500

    debut affichagefilm

--- td2.cpp
+++ td2.cpp
@@ -189,8 +189,7 @@ void afficherActeur(const Acteur& acteur)
 
 //TODO: Une fonction pour afficher un film avec tous ces acteurs (en utilisant la fonction afficherActeur ci-dessus).
 void afficherListeActeur(const Film* film) {
-	for (auto acteurExistantPtr : spanListeActeur(film->acteurs) {
-	}
+
 }
 
 
